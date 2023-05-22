15288e84fd14a5f4d173fcdf9f18cb7071a788db
Leudys Torres <leudystorres30@gmail.com>
2023-05-22 21:01:30 +0200

    js come back

--- app.js
+++ app.js
@@ -28,3 +28,11 @@ btns.forEach(function (btn) {
         value.textContent = count;
     });
 });
+
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Come Back ;( ";
+});
+window.addEventListener("focus", () => {
+    document.title = docTitle;
+});
